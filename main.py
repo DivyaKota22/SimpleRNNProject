d0c64780ab0f7d75057942e781134669785d86b5
Divya K <kotadivya0822@gmail.com>
2026-05-27 20:02:54 -0500

    Intial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+
+## Load the ImDB datset word index
+word_index=imdb.get_word_index()
+reverse_word_index= {value: key for key,value in word_index.items()}
+
+model=load_model('simple_rnn_model.h5')
+
+#Function to preprocess user input
+def preprocess_text(text):
+    words=text.lower().split()
+    encoded_review=[ word_index.get(word,2)+ 3 for word in words]
+    padded_review =sequence.pad_sequences([encoded_review],maxlen=500)
+    return padded_review
+
+### Prediction function
+def predict_sentiment(review):
+    preprocessed_input=preprocess_text(review)
+
+    prediction=model.predict(preprocessed_input)
+
+    sentiment= 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    return sentiment,prediction[0][0]
+
+###stream lit app
+import streamlit as st
+st.title('ImDB moview Review Sentiment Analaysis ')
+st.write('Enter a movie review to classify it as positive or negative.')
+
+#user input
+user_input=st.text_area('Movie Review')
+
+if st.button('Classify'):
+    preprocess_input=preprocess_text(user_input)
+
+
+    ## make prediction
+    prediction=model.predict(preprocess_input)
+    sentiment='Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    ## Display the result
+    st.write(f'SEntiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction[0][0]}')
+else:
+    st.write('Please enter a movie review.')
